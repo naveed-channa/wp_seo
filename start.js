9d81f1c33ca1f23ec0edd7518eb09d025345867b
naveed-channa <channanaveed6@gmail.com>
2022-11-08 10:32:18 +0500

    changes

--- start.js
+++ start.js
@@ -17,8 +17,8 @@ require('dotenv').config();
 // require('./models/Registration');
 const app = require('./app');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3001;
 
-const server = app.listen(3001, () => {
-  console.log(`Express is running on port ${port}`);
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode",port);
 });
